d4044bece1a8449dafe193ff12d067c48c4254e6
slaybourn <65286703+slaybourn@users.noreply.github.com>
2020-09-08 23:19:02 +0200

    Update WifiCheckInstall.sh

--- WifiCheckInstall.sh
+++ WifiCheckInstall.sh
@@ -1,5 +1,21 @@
-
 #!/bin/bash
+# first line tells it it's a shell script
+
+# Sets the file to be altered
+FILE="/usr/local/bin/wifiCheck.sh"
+
+# Nice friendly user message... don't panic / 42
+echo "Working on the Wifi Check Installer.....
+
+# bin/cat is the command we use, EOM is the "End of message"
+# we use to tell when the command is done (can be anything) 
+# $FILE calls the file we defined
+# The  <<EOM > send all text untill the "EOM" part to the file
+# Known as a "here document" for one > all is replaced in the file
+# to append, use >>$FILE
+# Followed by the the code we actually want to replace everything in the file with
+
+/bin/cat <<EOM >$FILE
 
 ping -c 4 1.1.1.1 > /dev/null
 
@@ -10,3 +26,23 @@ then
         sleep 5
         /sbin/ifconfig wlan0 up
 fi
+
+EOM
+
+# friendly message
+
+echo "Done"
+
+echo "Starting Crontab job generation"
+
+#Ask how often (in minutes) the job should run
+read -p "How many minutes to wait between each test (recommend  5 min, but 1 min is possible"  waitTime
+
+echo "Generating crontab job"
+
+
+#adding job to crontabs
+
+(crontab -l ; echo "/$waitTime * * * * /usr/bin/sudo -H /usr/local/bin/wifiCheck.sh >> /dev/null 2>&1")| crontab -
+
+echo "Done"
