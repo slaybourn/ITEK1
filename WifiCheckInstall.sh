cdaaa644cefe5a4baa2e42abf805e15e347a11f7
slaybourn <65286703+slaybourn@users.noreply.github.com>
2020-09-08 23:03:05 +0200

    Update WifiCheckInstall.sh

--- WifiCheckInstall.sh
+++ WifiCheckInstall.sh
@@ -1,1 +1,12 @@
 
+#!/bin/bash
+
+ping -c 4 1.1.1.1 > /dev/null
+
+if [ $? != 0 ]
+then
+        echo "No network connection, restarting wlan0"
+        /sbin/ifconfig wlan0 down
+        sleep 5
+        /sbin/ifconfig wlan0 up
+fi
